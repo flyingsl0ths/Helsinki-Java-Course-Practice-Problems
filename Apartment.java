62914090884b380c9e160791f2a402bfd302560b
flyingsl0ths <53455449+flyingsl0ths@users.noreply.github.com>
2020-01-17 14:07:20 -0600

    Added Exercise 87

---
+++ Apartment.java
@@ -0,0 +1,36 @@
+public class Apartment 
+{
+    private int rooms;
+    private int squareMeters;
+    private int pricePerSquareMeter;
+
+    public Apartment(int rooms, int squareMeters, int pricePerSquareMeter)
+    {
+        this.rooms = rooms;
+        this.squareMeters = squareMeters;
+        this.pricePerSquareMeter = pricePerSquareMeter;
+    }
+    
+    public boolean larger(Apartment otherApartment)
+    {
+        return this.squareMeters > otherApartment.squareMeters;
+    }
+    
+    public int priceDifference(Apartment otherApartment)
+    {
+        int priceOfThisApartment = this.squareMeters * this.pricePerSquareMeter;
+        
+        int priceOfOtherApartment = otherApartment.squareMeters * otherApartment.pricePerSquareMeter;
+        
+        return Math.abs(priceOfThisApartment - priceOfOtherApartment);
+    }
+    
+    public boolean moreExpensiveThan(Apartment otherApartment)
+    {
+        int priceOfThisApartment = this.squareMeters * this.pricePerSquareMeter;
+        
+        int priceOfOtherApartment = otherApartment.squareMeters * otherApartment.pricePerSquareMeter;
+        
+        return priceOfThisApartment > priceOfOtherApartment;
+    }
+}

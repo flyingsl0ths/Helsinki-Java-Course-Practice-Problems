c9f91fed14c4e5130c48363fd95bb812e494a5a8
flyingsl0ths <53455449+flyingsl0ths@users.noreply.github.com>
2020-01-17 14:08:39 -0600

    Added Exercise 88

---
+++ Student.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Student
+{
+    private String name;
+    private String studentNumber;
+    
+    public Student(String name, String studentNumber)
+    {
+        this.name = name;
+        this.studentNumber = studentNumber;
+    }
+    
+    public String getName()
+    {
+        return this.name;
+    }
+    
+    public String getStudentNumber()
+    {
+        return this.studentNumber;
+    }
+    
+    public String toString()
+    {
+        return this.name + " " + "(" + this.studentNumber + ")";
+    }
+    
+    public static void printAllStudentInfo(ArrayList<Student> list)
+    {
+        for(Student s : list)
+        {
+            System.out.println(s);
+        }
+        System.out.println();
+    }
+    
+    public static void main(String[] args) 
+    {
+        Scanner reader = new Scanner(System.in);
+        ArrayList<Student> studentList = new ArrayList<Student>();
+        String studentName;
+        String studentNumber;
+        
+        // Read in student names and numbers
+        System.out.print("Name: ");
+        studentName = reader.nextLine();
+        
+        // Add students (objects) to list
+        while (!studentName.isEmpty()) {
+            System.out.print("Student Number: ");
+            studentNumber = reader.nextLine();
+            studentList.add(new Student(studentName, studentNumber));
+            System.out.print("Name: ");
+            studentName = reader.nextLine();
+        }
+        
+        // Prints all students info
+        printAllStudentInfo(studentList);
+        
+        // Search for students by name
+        System.out.print("Give search term: ");
+        String searchTerm = reader.nextLine();
+        System.out.println("Result:");
+        
+        for(Student s : studentList)
+        {
+            if(s.getName().contains(searchTerm))
+            {
+                System.out.println(s);
+            }
+        }
+        
+   }
+}
